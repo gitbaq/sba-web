596a9909f5da143ceab71f980f84cc9f2f73d7df
SBee <gitbaq@gmail.com>
2025-06-10 17:30:05 +1000

    sidebar mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,18 @@ import type { Metadata } from "next";
 import type { Viewport } from "next";
 import "./ui/globals.css";
 import { inter } from "@/app/ui/fonts";
-import Footer from "@/components/footer";
-import { LearningSidebar } from "@/components/sidebar";
-import Navbar from "@/components/navbar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+// import Footer from "@/components/footer";
+// import { LearningSidebar } from "@/components/sidebar";
+// import Navbar from "@/components/navbar";
+// import { SidebarProvider } from "@/components/ui/sidebar";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
-import Rightbar from "@/components/rightbar";
+// import Rightbar from "@/components/rightbar";
 import { Toaster } from "@/components/ui/sonner";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "next-themes";
+import SidebarWrapper from "@/components/SidebarWrapper";
 export const metadata: Metadata = {
   title: "Syed B - Innovate, Lead, Succeed",
   description: "Syed B - Innovate, Lead, Succeed",
@@ -32,6 +33,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang='en' suppressHydrationWarning>
       <head>
@@ -47,23 +49,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
           <TooltipProvider delayDuration={2000}>
-            <SidebarProvider defaultOpen={false} className='h-full w-full'>
-              <div className='flex flex-col w-full h-full min-h-full'>
-                <Navbar />
-                <div className='flex flex-row w-full min-h-full h-full'>
-                  <LearningSidebar />
-                  <main className='flex flex-row w-full h-full items-start justify-center'>
-                    <div className='lg:max-w-4xl md:max-w-lg w-full h-full'>
-                      {children}
-                    </div>
-                  </main>
-                  <div className='md:flex hidden flex-col min-h-full w-64 min-w-64'>
-                    <Rightbar />
-                  </div>
-                </div>
-                <Footer />
-              </div>
-            </SidebarProvider>
+            <SidebarWrapper>{children}</SidebarWrapper>
           </TooltipProvider>
           <Toaster />
           <Analytics />
---
+++ components/SidebarWrapper.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React from 'react'
+import { SidebarProvider } from './ui/sidebar';
+import Footer from './footer';
+import Rightbar from './rightbar';
+import { LearningSidebar } from './sidebar';
+import Navbar from './navbar';
+import { useIsMobile } from '@/hooks/use-mobile';
+
+export default function SidebarWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+    const isMobile = useIsMobile();
+  return (
+    <SidebarProvider defaultOpen={!isMobile} className='h-full w-full'>
+      <div className='flex flex-col w-full h-full min-h-full'>
+        <Navbar />
+        <div className='flex flex-row w-full min-h-full h-full'>
+          <LearningSidebar />
+          <main className='flex flex-row w-full h-full items-start justify-center'>
+            <div className='lg:max-w-4xl md:max-w-lg w-full h-full'>
+              {children}
+            </div>
+          </main>
+          <div className='md:flex hidden flex-col min-h-full w-64 min-w-64'>
+            <Rightbar />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </SidebarProvider>
+  );
+}
+
