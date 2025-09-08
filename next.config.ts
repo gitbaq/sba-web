7162af7c8ca392c4fd2bf80b7c721cfb8328170b
SBee <gitbaq@gmail.com>
2025-09-08 22:55:01 +1000

    next config reverted

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   crossOrigin: "anonymous",
-  outputFileTracingRoot:
-    "/Users/syedbaqirali/src/_prod_projects/sbaweb/sbaweb_frontend",
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
+};
+
+module.exports = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "sbaweb-bucket.s3.ap-southeast-2.amazonaws.com",
-        pathname: "/**",
-      },
+      new URL("https://sbaweb-bucket.s3.ap-southeast-2.amazonaws.com/**"),
     ],
   },
 };
