333d68a278702764b88bb155b76a8aec1c55e23d
SBee <gitbaq@gmail.com>
2025-10-28 12:38:59 +1100

    CB Images in config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ module.exports = {
       new URL("https://sbaweb-bucket.s3.ap-southeast-2.amazonaws.com/**"),
       new URL("https://ai.syedbaqirali.com/**"),
       new URL("https://www.syedbaqirali.com/**"),
+      new URL("https://www.codingburo.com/**"),
     ],
   },
 };
